865bd5b658d4f24aa1226880fa143a53586f8c7f
toancita <antomartinamanzanares@gmail.com>
2025-06-15 23:09:28 -0400

    Feat: Add hero

---
+++ src/app/_components/hero.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Link from 'next/link';
+import { FiSearch } from 'react-icons/fi';
+import styles from './hero.module.css';
+
+export default function Hero() { 
+  return (
+    <div className={styles.heroContainer}>
+      <header className={styles.header}>
+        <h1 className={styles.logo}>momentum</h1>
+        <nav className={styles.nav}>
+          <Link href="/github" className={styles.link}>Github</Link>
+          <Link href="/about" className={styles.button}>About us</Link>
+        </nav>
+      </header>
+
+      <div className={styles.textContainer}>
+        <h2 className={styles.mainText}>Discover the best photos.</h2>
+        <p className={styles.subText}>Boost your creativity.</p>
+
+        <div className={styles.searchContainer}>
+          <input
+            type="text"
+            placeholder="Search for free photos"
+            className={styles.searchInput}
+          />
+          <FiSearch className={styles.searchIcon} />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,16 @@
 import "./globals.css";
 import Footer from "./_components/footer";
+import Hero from "./_components/hero";
 import NavBar from "./_components/navigationBar";
+import localFont from 'next/font/local';
+import './globals.css';
+
 
 export default function DashboardLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Hero />
         <NavBar />
         <main>{children}</main>
         <Footer />
